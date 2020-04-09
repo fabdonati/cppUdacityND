000ec0c763a9fd2658b0ef63bde0360fe176e56f
fab <fabriziovt@gmail.com>
2020-04-09 11:40:47 +0200

    added project1 to repo

---
+++ project1/build/thirdparty/googletest/googlemock/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/fabrizio/dev/RoutePlannerProject/thirdparty/googletest/googlemock
+# Build directory: /home/fabrizio/dev/RoutePlannerProject/build/thirdparty/googletest/googlemock
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
